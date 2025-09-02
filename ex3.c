1a64edd9e973d70b7666e448afc0d4ed4179277f
safae <safaebou@gmail.com>
2025-09-02 16:39:52 +0200

    ex3

---
+++ ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+printf("1.              *\n");
+printf("2.            *\n");
+printf("3.           *\n");
+printf("4.  *      *\n");
+printf("5.    *   *\n");
+printf("6.      *\n");
+return 0;
+
+  }
