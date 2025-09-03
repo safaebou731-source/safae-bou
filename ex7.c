cee1c1f51dfbacf7c4c56ebb82e05dfe2fa3f1fe
safae <safaebou@gmail.com>
2025-09-03 12:00:56 +0200

    ex7

--- ex7.c
+++ ex7.c
@@ -3,12 +3,7 @@ int main(void){
     int x,p;
     printf("veuiller entrer la valeur de x :");
     scanf("%d",&x);
-    p=3*x*x*x*x*x
-    +2*x*x*x*x
-    -5*x*x*x
-    -x*x
-    +7*x
-    -6;
+    p=((((3*x+2)*x-5)*x-1)*x+7)*x-6;
     printf("la valeur du polynome est %d \n",p);
       
 
