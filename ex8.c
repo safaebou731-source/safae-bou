d90c6bb192576b837baa5598c368407186572a2d
safae <safaebou@gmail.com>
2025-09-03 12:13:02 +0200

    ex8

---
+++ ex8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void){
+    int x,p;
+    printf("veuiller entrer la valeur de x :");
+    scanf("%d",&x);
+    3*x*x*x*x*x
+    +2*x*x*x*x
+    -5*x*x*x
+    -x*x
+    +7*x
+    -6;
+    printf("la valeur du polynome est %d \n",p);
+       
+    return 0;
+
+
+
+
+
+
+
+
+}
